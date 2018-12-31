ca8a6e90487d9d8f798fba284124916edd479a25
Atul Prakash Singh <simplifypython@gmail.com>
2018-12-31 16:42:03 +0530

    Modified 1_single_image_plotting with matplotlib.image library insteas of keras.preprocessing.image

--- 1_single_image_plotting.py
+++ 1_single_image_plotting.py
@@ -1,31 +1,16 @@
 import matplotlib.pyplot as plt
-from keras.preprocessing import image
+import matplotlib.image as mpimg
 
-
-# path to training & testing dataset
+# path to image
 image_path = 'images/a1.jpeg'
 
+# load image as NumPy Array
+img_as_arr = mpimg.imread(image_path)
 
-# image datasets variables
-# image_size = (28, 28)
-# colorscale = 'rgb'  # grayscale
-
-# load image
-img = image.load_img(image_path)
-# or
-# img = image.load_img(image_path, target_size=image_size)
-
-
-print(img)
-
-
-img_as_arr = image.img_to_array(img)
+# print Array shape
 print(img_as_arr.shape)
 
-
-img_as_flatten_arr = img_as_arr.flatten()
-print(img_as_flatten_arr.shape)
-
+# show image
 plt.imshow(img_as_arr/255)
 plt.show()
 # plt.savefig("image1.jpg")
